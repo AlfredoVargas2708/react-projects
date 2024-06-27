aab3d58571dfb75ddecdd1eb50ffb04c59263796
VargasGandolini <avg072023@gmail.com>
2024-06-27 14:28:19 -0400

    Decimales

--- calculator/src/App.jsx
+++ calculator/src/App.jsx
@@ -12,19 +12,75 @@ import "./App.css";
 import { useState } from "react";
 
 function App() {
-  const [inicial, setInicial] = useState(0);
-  const [numero, setNumero] = useState(0);
+  let [numero, setNumero] = useState([]);
+  let [formula, setFormula] = useState("");
+  let [resultadoFinal, setResultadoFinal] = useState(0);
+
   function restard() {
-    setInicial(0);
+    setNumero([]);
+    setFormula("");
+    setResultadoFinal(0);
   }
 
-  function agregarNumero() {
-    if (inicial === 0) {
-      setInicial(Number(numero));
-    } else {
-      setInicial(Number(inicial) + Number(numero));
+  function formulas(e) {
+    if (e.target.className === "suma") {
+      setFormula("suma");
+    } else if (e.target.className === "resta") {
+      setFormula("resta");
+    } else if (e.target.className === "multiplicacion") {
+      setFormula("multiplicacion");
+    } else if (e.target.className === "division") {
+      setFormula("division");
+    } else if (e.target.className === "modulo") {
+      setFormula("modulo");
     }
-    setNumero(0);
+  }
+
+  function mostrar() {
+    let i = 0;
+    let decimal = 0;
+    let result = [];
+
+    for (let i = 0; i < numero.length; i++) {
+      if (numero[i] === ".") {
+        decimal = numero[i - 1] + numero[i] + numero[i + 1];
+        result.push(decimal);
+        if (i > 0 && !numero[i - 1].includes(".")) {
+          numero[i - 1] = null;
+        }
+        if (i < numero.length && !numero[i + 1].includes(".")) {
+          numero[i + 1] = null;
+        }
+      }
+    }
+    let filteredArray = numero.filter(
+      (element) => element !== null && element !== "."
+    );
+    if (filteredArray.length > 0) {
+      result.push(filteredArray);
+    }
+
+    result = result.filter(
+      (element) => !(Array.isArray(element) && element.length === 0)
+    );
+
+    console.log(result);
+    while (i < result.length) {
+      if (formula === "suma") {
+        resultadoFinal = resultadoFinal + Number(result[i]);
+      } else if (formula === "resta") {
+        resultadoFinal = resultadoFinal - Number(result[i]);
+      } else if (formula === "multiplicacion") {
+        resultadoFinal = resultadoFinal * Number(result[i]);
+      } else if (formula === "division") {
+        resultadoFinal = resultadoFinal / Number(result[i]);
+      } else if (formula === "modulo") {
+        resultadoFinal = resultadoFinal % Number(result[i]);
+      }
+      i++;
+    }
+    setResultadoFinal(resultadoFinal);
+    setNumero([]);
   }
 
   return (
@@ -37,8 +93,8 @@ function App() {
         <input
           id="calculo"
           type="text"
-          value={inicial}
-          onChange={(e) => setInicial(e.target.value)}
+          value={resultadoFinal}
+          onChange={(e) => setResultadoFinal(e.target.value)}
         />
       </section>
       <section className="first-row">
@@ -46,15 +102,15 @@ function App() {
           {" "}
           <TbLetterC />{" "}
         </button>
-        <button className="parentsis">
+        <button className="parentesis">
           {" "}
           <LuParentheses />{" "}
         </button>
-        <button className="modulo">
+        <button className="modulo" onClick={formulas}>
           {" "}
           <CgMathPercent />{" "}
         </button>
-        <button className="division">
+        <button className="division" onClick={formulas}>
           {" "}
           <CgMathDivide />{" "}
         </button>
@@ -62,26 +118,32 @@ function App() {
       <section className="second-row">
         <button
           className="numero7"
-          onClick={(e) => setNumero(e.target.innerText)}
+          onClick={(e) =>
+            setNumero((prevState) => [...prevState, e.target.innerText])
+          }
         >
           {" "}
           7{" "}
         </button>
         <button
           className="numero8"
-          onClick={(e) => setNumero(e.target.innerText)}
+          onClick={(e) =>
+            setNumero((prevState) => [...prevState, e.target.innerText])
+          }
         >
           {" "}
           8{" "}
         </button>
         <button
           className="numero9"
-          onClick={(e) => setNumero(e.target.innerText)}
+          onClick={(e) =>
+            setNumero((prevState) => [...prevState, e.target.innerText])
+          }
         >
           {" "}
           9{" "}
         </button>
-        <button className="multplicar">
+        <button className="multiplicacion" onClick={formulas}>
           {" "}
           <LiaTimesSolid />{" "}
         </button>
@@ -89,26 +151,32 @@ function App() {
       <section className="third-row">
         <button
           className="numero4"
-          onClick={(e) => setNumero(e.target.innerText)}
+          onClick={(e) =>
+            setNumero((prevState) => [...prevState, e.target.innerText])
+          }
         >
           {" "}
           4{" "}
         </button>
         <button
           className="numero5"
-          onClick={(e) => setNumero(e.target.innerText)}
+          onClick={(e) =>
+            setNumero((prevState) => [...prevState, e.target.innerText])
+          }
         >
           {" "}
           5{" "}
         </button>
         <button
           className="numero6"
-          onClick={(e) => setNumero(e.target.innerText)}
+          onClick={(e) =>
+            setNumero((prevState) => [...prevState, e.target.innerText])
+          }
         >
           {" "}
           6{" "}
         </button>
-        <button className="resta">
+        <button className="resta" onClick={formulas}>
           {" "}
           <CgMathMinus />{" "}
         </button>
@@ -116,26 +184,32 @@ function App() {
       <section className="fourth-row">
         <button
           className="numero1"
-          onClick={(e) => setNumero(e.target.innerText)}
+          onClick={(e) =>
+            setNumero((prevState) => [...prevState, e.target.innerText])
+          }
         >
           {" "}
           1{" "}
         </button>
         <button
           className="numero2"
-          onClick={(e) => setNumero(e.target.innerText)}
+          onClick={(e) =>
+            setNumero((prevState) => [...prevState, e.target.innerText])
+          }
         >
           {" "}
           2{" "}
         </button>
         <button
           className="numero3"
-          onClick={(e) => setNumero(e.target.innerText)}
+          onClick={(e) =>
+            setNumero((prevState) => [...prevState, e.target.innerText])
+          }
         >
           {" "}
           3{" "}
         </button>
-        <button className="suma" onClick={agregarNumero}>
+        <button className="suma" onClick={formulas}>
           {" "}
           <CgMathPlus />{" "}
         </button>
@@ -143,8 +217,16 @@ function App() {
       <section className="fifth-row">
         <button className="mas-menos"> +/- </button>
         <button className="numero0"> 0 </button>
-        <button className="punto"> . </button>
-        <button className="igual">
+        <button
+          className="punto"
+          onClick={(e) =>
+            setNumero((prevState) => [...prevState, e.target.innerText])
+          }
+        >
+          {" "}
+          .{" "}
+        </button>
+        <button className="igual" onClick={mostrar}>
           {" "}
           <CgMathEqual />{" "}
         </button>
