e1f8cb37b058de208aa3f4eed12cd01485ea2ec2
VargasGandolini <avg072023@gmail.com>
2024-06-28 15:28:36 -0400

    Hola

--- Hooks/hooks-react/src/App.jsx
+++ Hooks/hooks-react/src/App.jsx
@@ -1,33 +1,7 @@
-import CharacterCounter from "./Ejercicos_UseState/CharacterCounter";
-import ColowSwitcher from "./Ejercicos_UseState/ColowSwitcher";
-import Contador from "./Ejercicos_UseState/Contador";
-import DragDropList from "./Ejercicos_UseState/DragDropList";
-import InputField from "./Ejercicos_UseState/InputField";
-import SearchFilter from "./Ejercicos_UseState/SearchFilter";
-import TodoList from "./Ejercicos_UseState/TodoList";
-import ToggleVisibility from "./Ejercicos_UseState/ToggleVisibility";
+import React from "react";
 
 const App = () => {
-  return (
-    <div>
-      {" "}
-      <Contador />
-      <hr />
-      <InputField />
-      <hr />
-      <ToggleVisibility />
-      <hr />
-      <CharacterCounter />
-      <hr />
-      <TodoList />
-      <hr />
-      <ColowSwitcher />
-      <hr />
-      <SearchFilter />
-      <hr />
-      <DragDropList />
-    </div>
-  );
+  return <div>App</div>;
 };
 
 export default App;
